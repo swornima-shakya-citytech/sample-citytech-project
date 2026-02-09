b4c41530210476f6a037fed653e01ff9273951ed
Swornima Shakya <swornima.shakya@citytech.global>
2026-02-09 11:05:16 +0545

    Create populate_repo.sh with instructions to run Python script for dummy data.

---
+++ populate_repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Instructions to run the Python script to populate the repository
+
+# 1. Ensure you have Python installed in your system.
+# 2. Install the required packages:
+#    pip install requests
+
+# 3. Place your Python script (populate_data.py) in the same directory.
+# 4. To run the script, use the following command:
+#    python populate_data.py
+
+# This will populate the repository with dummy data using the GitHub API.
